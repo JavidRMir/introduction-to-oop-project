dbf96132fcbc2e12cc2785dae1bf9f1d8ac07b24
Javid Mir <mir.javid@gmail.com>
2021-09-07 10:49:27 +0100

    Activity 2 - all excercises

---
+++ src/com/elitespectra/Main.java
@@ -0,0 +1,12 @@
+package com.elitespectra;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bird myBird = new Bird("Tweety", "Yellow");
+        myBird.speak();
+        myBird.fly();
+
+    }
+}
